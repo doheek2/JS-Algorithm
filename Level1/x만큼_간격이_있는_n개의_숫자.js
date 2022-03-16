44232d697bb9b066c7aa52ff9a71ab2de59c548e
doheek2 <doheek2@naver.com>
2022-03-16 18:58:36 +0900

    Programmers Algorithm Level1

---
+++ Level1/x만큼_간격이_있는_n개의_숫자.js
@@ -0,0 +1,13 @@
+function solution(x, n) {
+    let arr = [];
+    for(let i = 1; i <= n; i++) {
+        arr.push(x*i);
+    }
+    return arr;
+}
+
+/*
+function solution(x, n) {
+    return Array(n).fill(x).map((v, i) => (i + 1) * v)
+}
+*/
